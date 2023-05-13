0738a1878c68f090a9850dc4a6ea27faf88e6983
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2023-05-13 13:15:20 +0400

    split dashboard into individual components

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,7 @@
+import Stats from './Stats';
+import AllJobs from './AllJobs';
+import Profile from './Profile';
+import AddJob from './AddJob';
+import SharedLayout from './SharedLayout';
+
+export { SharedLayout, AddJob, AllJobs, Profile, Stats };
