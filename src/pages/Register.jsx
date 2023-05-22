ea54e85af4c4ae6f80dfdb879437afbb8ee71f78
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2023-05-22 21:10:10 +0400

    added demo user  for testing purposes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -87,6 +87,23 @@ const Register = () => {
 				>
 					{isLoading ? 'loading...' : 'submit'}
 				</button>
+				{values.isMember && (
+					<button
+						type="submit"
+						className="btn btn-block btn-hipster"
+						disabled={isLoading}
+						onClick={() =>
+							dispatch(
+								loginUser({
+									email: 'testUser@test.com',
+									password: 'secret',
+								})
+							)
+						}
+					>
+						{isLoading ? 'loading...' : 'demo app'}
+					</button>
+				)}
 				<p>
 					{values.isMember ? 'Not a member yet?' : 'Already a member?'}
 					<button
